45354d4096f9e0189231b78316e0f97905391357
Adrien <guedesadrien@yahoo.fr>
2021-02-18 11:44:57 +0100

    exo done

---
+++ app/src/main/java/com/gadrien456/gmail/counter/CalcActivity.java
@@ -0,0 +1,59 @@
+package com.gadrien456.gmail.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gadrien456.gmail.counter.databinding.ActivityMainBinding;
+
+public class CalcActivity extends AppCompatActivity {
+
+    private Integer counter = 0;
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initViews();
+
+    }
+    private void initViews() {
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                counter ++;
+                binding.counter.setText(counter.toString());
+            }
+        });
+        binding.toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(CalcActivity.this, binding.counter.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.computeButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                double op1 =  Double.parseDouble(String.valueOf(binding.firstOpField.getText()));
+                double op2 =  Double.parseDouble(String.valueOf(binding.secondOpField.getText()));
+                double calcResult = op1 + op2;
+                Toast.makeText(CalcActivity.this, Double.toString(calcResult),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/com/gadrien456/gmail/counter/MailActivity.java
+++ app/src/main/java/com/gadrien456/gmail/counter/MailActivity.java
@@ -11,40 +11,35 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gadrien456.gmail.counter.databinding.ActivityMainBinding;
+import com.gadrien456.gmail.counter.databinding.MailActivityBinding;
 import com.gadrien456.gmail.counter.databinding.NavActivityBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MailActivity extends AppCompatActivity {
 
-    NavActivityBinding binding;
+    MailActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
-        binding = NavActivityBinding.inflate(getLayoutInflater());
+        binding = MailActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         initViews();
 
     }
     private void initViews() {
 
-        binding.goToCalc.setOnClickListener(new View.OnClickListener() {
+        binding.sendMail.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
 
-                Intent calcIntent = new Intent(MainActivity.this,CalcActivity.class);
-                startActivity(calcIntent);
-            }
-        });
-        binding.goToMail.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View view) {
-
-                Intent calcIntent = new Intent(MainActivity.this,MailActivity.class);
-                startActivity(calcIntent);
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_TEXT, binding.mailText.getText());
+                email.setType("message/rfc822");
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"test@gmail.com","tist@gmail.com","tastast@gmail.com"});
+                startActivity(Intent.createChooser(email,"choose mail"));
             }
         });
 
